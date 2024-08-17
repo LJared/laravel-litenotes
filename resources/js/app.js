e6f4234379b0657f7f2d2b03d9bbf14107ec6b18
Jared García <ljared.31g@gmail.com>
2024-08-17 11:58:12 -0600

    fix: theme selection is correctly loaded when the page is loaded too

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,14 @@ var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
 // Apply the correct theme when the page loads.
 // If the user has set a preferred theme in localStorage, use that. Otherwise, use the OS preference.
-if (localStorage.getItem("color-theme") === "dark" || window.matchMedia("(prefers-color-scheme: dark)").matches) {
+if (localStorage.getItem("color-theme") === "dark") {
+    document.documentElement.classList.add("dark");
+    themeToggleLightIcon.classList.remove("hidden");
+} else if (localStorage.getItem("color-theme") === "light") {
+    document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon.classList.remove("hidden");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    // If no preference is stored, use the system's default theme
     document.documentElement.classList.add("dark");
     themeToggleLightIcon.classList.remove("hidden");
 } else {
