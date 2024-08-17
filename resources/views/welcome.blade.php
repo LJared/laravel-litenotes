f866bc24a8dc12188e7af4e27d17549e775812d3
Jared García <ljared.31g@gmail.com>
2024-08-17 13:32:23 -0600

    delete: all references to dashboard view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,10 @@
                 <nav class="w-full sm:absolute sm:top-0 sm:right-0 flex justify-end">
                     @auth
                         <a
-                            href="{{ url('/dashboard') }}"
+                            href="{{ route('notes.index') }}"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
-                            Dashboard
+                            Notes
                         </a>
                     @else
                         <a
