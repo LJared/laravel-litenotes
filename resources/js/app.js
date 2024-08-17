d01fec45f04e7e0bb3fec8ff709147ae0aa01f7b
Jared García <ljared.31g@gmail.com>
2024-08-17 10:40:50 -0600

    feat: added color theme switch button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,37 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Get both the light and dark toggle icons
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Apply the correct theme when the page loads.
+// If the user has set a preferred theme in localStorage, use that. Otherwise, use the OS preference.
+if (localStorage.getItem("color-theme") === "dark" || window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.classList.add("dark");
+    themeToggleLightIcon.classList.remove("hidden");
+} else {
+    document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+    // Toggle icons
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    // Switch the theme and save the current preference
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+    }
+});
