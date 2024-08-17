e2b579893e2304a4d0cd4941cd5c7134407a7742
Jared García <ljared.31g@gmail.com>
2024-08-17 13:59:06 -0600

    feat: added create button to notes.index

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -7,6 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-white">
+
+            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">Add new note</a>
+
             @forelse ($notes as $note)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 dark:bg-gray-800 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2xl">{{ $note->title }}</h2>
