08383f56d28c770fc77415a0314cf944eb2d17e2
Jared García <ljared.31g@gmail.com>
2024-09-07 12:32:43 -0600

    feat: created and implemented link-button component

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -8,7 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:text-white">
 
-            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">Add new note</a>
+            <x-link-button href="{{ route('notes.create') }}">
+                + New note
+            </x-link-button>
 
             @forelse ($notes as $note)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 dark:bg-gray-800 shadow-sm sm:rounded-lg">
