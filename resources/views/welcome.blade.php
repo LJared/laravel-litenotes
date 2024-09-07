28b69ef5f58952877d8d6f2e3fd39fe569716662
Jared García <ljared.31g@gmail.com>
2024-09-07 11:54:16 -0600

    ref(welcome view): removed unnecessary classes and tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,54 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name') }}</title>
 
-        <!-- Styles -->
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <!-- Navigation -->
-            @if (Route::has('login'))
-                <nav class="w-full sm:absolute sm:top-0 sm:right-0 flex justify-end">
-                    @auth
-                        <a
-                            href="{{ route('notes.index') }}"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Notes
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Log in
-                        </a>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-            <h1 class="text-5xl mt-6 sm:mt-0">
-                LiteNotes
-            </h1>
+<body class="font-sans antialiased min-h-screen grid place-items-center dark:text-white bg-gray-100 dark:bg-gray-900">
+    @if (Route::has('login'))
+        <div class="absolute top-0 right-0 p-6">
+            @auth
+                <a href="{{ route('notes.index') }}" class="text-indigo-600 hover:text-indigo-800 dark:hover:text-white/80 dark:text-white">
+                    Notes
+                </a>
+            @else
+                <a href="{{ route('login') }}" class="text-indigo-600 hover:text-indigo-800 mr-4 dark:hover:text-white/80 dark:text-white">
+                    Log in
+                </a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-800 dark:hover:text-white/80 dark:text-white">
+                        Register
+                    </a>
+                @endif
+            @endauth
         </div>
-    </body>
+    @endif
+    <h1 class="text-7xl">LiteNotes</h1>
+</body>
+
 </html>
