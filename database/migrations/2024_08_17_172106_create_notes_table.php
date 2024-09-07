3a28eea0462b5120aead7c7afbb6cc16662c53a9
Jared García <ljared.31g@gmail.com>
2024-09-07 11:54:57 -0600

    ref(notes migration): using foreignIdFor method instead of manually added foreign key field name

--- database/migrations/2024_08_17_172106_create_notes_table.php
+++ database/migrations/2024_08_17_172106_create_notes_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,9 +14,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
             $table->string('title');
             $table->longText('text');
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
