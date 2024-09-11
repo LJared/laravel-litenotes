25ca4632789f4f93fcea43493c2edeaf552dd1f9
Jared García <ljared.31g@gmail.com>
2024-09-11 14:13:04 -0600

    ref: delete single routes with grouped ones

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,6 @@ Route::resource('/notebooks', NotebookController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
 
-Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
-Route::get('/trashed/{note}', [TrashedNoteController::class, 'show'])->withTrashed()->middleware('auth')->name('trashed.show');
-Route::put('/trashed/{note}', [TrashedNoteController::class, 'update'])->withTrashed()->middleware('auth')->name('trashed.update');
-Route::delete('/trashed/{note}', [TrashedNoteController::class, 'destroy'])->withTrashed()->middleware('auth')->name('trashed.destroy');
-
 Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function () {
     Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
     Route::get('/{note}', [TrashedNoteController::class, 'show'])->withTrashed()->name('show');
